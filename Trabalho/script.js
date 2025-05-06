772fed03a02b963640249093c5b4bff5386e87af
CaelSoares <caelfelipesoares@gmail.com>
2025-05-06 14:16:15 -0300

    Add files via upload

---
+++ Trabalho/script.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector("#botao");
+let mensagem = document.querySelector("#mensagem");
+
+botao.addEventListener("click", exibirmensagem);
+function exibirmensagem(){
+   mensagem.innerHTML = "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna. João 3:16";
+}
+   
